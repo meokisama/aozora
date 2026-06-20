ca65a1e5ab36374474c042cc0b5e9ee9bd0c3d9c
meokisama <hoangdinhsang123@gmail.com>
2026-06-20 22:15:41 +0700

    chore: initial commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Toaster } from "@/components/ui/sonner";
+
+export function App() {
+  return (
+    <div>
+      <p>Hello World!</p>
+      <Toaster />
+    </div>
+  );
+}
