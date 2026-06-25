f98ff766fe008529c0e482e36b760ee004ec9d60
meokisama <hoangdinhsang123@gmail.com>
2026-06-25 10:14:46 +0700

    chore: mimimize file path

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,14 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
     },
   },
   build: {
